b7e42a3e045e04b60293eb1cecc03919361b6a8c
Kei <koka0828us@gmail.com>
2023-05-30 12:00:00 +0900

    Update web package

--- web/next.config.js
+++ web/next.config.js
@@ -4,6 +4,18 @@ import webpack from 'webpack';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.ttf$/,
