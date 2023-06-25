c06534dc3c6433560549e6506337c953a402f3b8
Kei <koka0828us@gmail.com>
2023-06-25 12:00:00 +0900

    Update web package

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,14 +1,30 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import { Inter, Poppins, M_PLUS_Rounded_1c } from 'next/font/google';
 import { createTheme, ThemeProvider } from '@mui/material';
+import 'reset-css';
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
 
+const poppins = Poppins({
+  variable: '--font-poppins',
+  weight: ["300", "400", "500", "600"],
+  subsets: ['latin'],
+})
+
+const mPlusRounded1c = M_PLUS_Rounded_1c({
+  variable: '--font-m-plus-rounded-1c',
+  weight: ["300", "400", "500", "700"],
+  subsets: ['latin'],
+})
+
 const theme = createTheme({
+  typography: {
+    fontFamily: 'var(--font-poppins)',
+  },
   palette: {
     primary: {
       main: '#9FD1D5',
@@ -46,10 +62,10 @@ const theme = createTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <main className={inter.variable}>
+      <main className={`${poppins.variable} ${mPlusRounded1c.variable}`}>
         <Component {...pageProps} />
       </main>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 }
 
