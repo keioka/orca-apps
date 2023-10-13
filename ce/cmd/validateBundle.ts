b6c7e3decd0c00afa8b64efbda7a63c355039a5b
Kei <koka0828us@gmail.com>
2023-10-13 12:00:00 +0900

    Update extension index

--- ce/cmd/validateBundle.ts
+++ ce/cmd/validateBundle.ts
@@ -11,46 +11,49 @@ const DIRECTORY_PATHS = {
 const UNWANTED_CODE = 'document.createElement("script")';
 
 function validateFile(filePath, code = UNWANTED_CODE) {
+  console.log(`Validating file ${filePath}...`);
   const fileContent = fs.readFileSync(filePath, 'utf8');
 
   if (fileContent.includes(code)) {
     console.error(`❌ Error: Unwanted code found in the file \n ${filePath}. \n`);
-    return false;
+    return false
   }
 
-  return true;
+  return true
 }
 
 function validateAllFiles(directoryPath) {
   const files = fs.readdirSync(directoryPath);
   let validationPassed = true;
-
   for (const file of files) {
+    console.log(file)
     const filePath = path.join(directoryPath, file);
     const stat = fs.statSync(filePath);
 
-    if (stat.isDirectory()) {
-      if (!validateAllFiles(filePath)) { // Recursive call
+    if (stat.isFile() && filePath.endsWith('.js')) {
+      if (!validateFile(filePath)) {
         validationPassed = false;
       }
-    } else if (stat.isFile() && filePath.endsWith('.js')) {
-      if (!validateFile(filePath)) {
+    }
+
+    if (stat.isDirectory()) {
+      if (!validateAllFiles(filePath)) {
+        console.log(`❌ filePath: ${filePath} ❌`);
         validationPassed = false;
       }
     }
   }
 
-  return validationPassed;
-}
-
-if (DIRECTORY_PATHS[env]) {
-  console.log(`Validating files in the directory ${DIRECTORY_PATHS[env]}...`);
-  const result = validateAllFiles(DIRECTORY_PATHS[env]);
-  if (!result) {
+  if (!validationPassed) {
     console.log(`❌❌❌ Validation failed for ${env} environment: Unwanted code found in the files. ❌❌❌`);
   } else {
     console.log(`✅ Validation passed for ${env} environment: No unwanted code found in the files.`);
   }
+}
+
+if (DIRECTORY_PATHS[env]) {
+  console.log(`Validating files in the directory ${DIRECTORY_PATHS[env]}...`);
+  validateAllFiles(DIRECTORY_PATHS[env]);
 } else {
   console.log(`Unknown environment: ${env}. Skipping validation.`);
 }
