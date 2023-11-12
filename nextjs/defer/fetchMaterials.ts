c1455cefa0f2d3fb6feada1dd702e1a2be2b81e3
Kei <koka0828us@gmail.com>
2023-11-12 12:00:00 +0900

    Update web fetchMaterials

--- nextjs/defer/fetchMaterials.ts
+++ nextjs/defer/fetchMaterials.ts
@@ -4,14 +4,17 @@ import createArticlesByPublisherId from "@/defer/createArticlesByPublisherId";
 
 async function fetchMaterialsEveryFiveMinutes() {
   // business logic here
+  console.log("Fetching materials every 5 minutes starts");
   const publishers = await checkPublishersCrawledStatus()
   const publishersToCrawl = publishers.slice(0, 50)
-
+  console.log({
+    createArticlesByPublisherId
+  })
   await Promise.all(publishersToCrawl.map(async (publisher) => {
     await createArticlesByPublisherId({ publisherId: publisher.id })
-  })
+  }))
 
-  console.log("Fetching materials every 5 minutes");
+  console.log("Fetching materials every 5 minute ends");
 }
 
 export default defer.cron(fetchMaterialsEveryFiveMinutes, "*/5 * * * *");
