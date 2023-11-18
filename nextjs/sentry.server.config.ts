bc34b18f384506aecde7c0e31e3c8624d0c9b88e
Kei <koka0828us@gmail.com>
2023-11-18 12:00:00 +0900

    Add new API

---
+++ nextjs/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e6c7a1fb1e248b14907c173f425b0617@o4506180296966144.ingest.sentry.io/4506180328947712",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
