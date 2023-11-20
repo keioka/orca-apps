dabf932eac7728377e9721297f64680d947759de
Kei <koka0828us@gmail.com>
2023-11-20 12:00:00 +0900

    Update mobile translations

--- rn/locales/index.ts
+++ rn/locales/index.ts
@@ -1,5 +1,4 @@
-import { I18nManager } from 'react-native';
-import * as Localization from 'expo-localization';
+// import * as Localization from 'expo-localization';
 import { I18n } from "i18n-js";
 import en from './en.json'
 import ja from './ja.json'
@@ -9,6 +8,5 @@ export const i18n = new I18n({
   ja: ja,
 });
 
-i18n.locale = Localization.locale.split('-')[0];
 
-console.log("Localization.locale", Localization.locale)
+i18n.locale = "ja"
