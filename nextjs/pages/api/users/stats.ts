cc295c99da86132ab0399aacf5376ce472bdc9d9
Kei <koka0828us@gmail.com>
2023-11-22 12:00:00 +0900

    Add progress

---
+++ nextjs/pages/api/users/stats.ts
@@ -0,0 +1,106 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/db';
+import { validateToken } from '@/firebase';
+import { setCurrentUser } from '@/middleware/setCurrentUser';
+import moment from 'moment';
+
+type StatsByDay = {
+  [key: string]: number;
+};
+
+type ApiResponse = {
+  lessonsByDay: StatsByDay;
+  vocabulariesByDay: StatsByDay;
+  paraphrasesByDay: StatsByDay;
+  wordCountByDay: StatsByDay;
+};
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse | { error: string }>
+) {
+  if (req.method === 'GET') {
+    try {
+      await validateToken(req, res)
+      await setCurrentUser(req, res)
+    } catch (err) {
+      console.error(err)
+      return res.status(401).json({ error: { code: "AUTH/NOT_FOUND", message: 'AUTH_NOT_FOUND' } });
+    }
+
+    const { currentUser } = req
+    const userId = currentUser.userId
+
+    try {
+      // Retrieve data for each category
+      const lessons = await prisma.lesson.findMany({
+        where: { userId },
+        select: { createdAt: true }
+      });
+      const vocabularies = await prisma.savedVocabularies.findMany({
+        where: { userId },
+        select: { createdAt: true }
+      });
+      const paraphrases = await prisma.savedParaphrase.findMany({
+        where: { userId },
+        select: { createdAt: true }
+      });
+      const messages = await prisma.message.findMany({
+        where: { createdById: userId, type: 'user' },
+        select: { createdAt: true, content: true }
+      });
+
+      // Helper function to format date as YYYY-MM-DD
+      const formatDate = (date) => date.toISOString().split('T')[0];
+
+      // Group data by day
+      const groupByDay = (data) => {
+        return data.reduce((acc, item) => {
+          const date = formatDate(item.createdAt);
+          acc[date] = acc[date] || [];
+          acc[date].push(item);
+          return acc;
+        }, {});
+      };
+
+      const getCountByDay = (dataByDay) => {
+        return Object.keys(dataByDay).map((date) => ({
+          date: new Date(date),
+          label: moment(date).format('MM/DD'),
+          value: dataByDay[date].length
+        }))
+      }
+
+      // Group each category by day
+      const lessonsByDay = groupByDay(lessons);
+      const vocabulariesByDay = groupByDay(vocabularies);
+      const paraphrasesByDay = groupByDay(paraphrases);
+      const messagesByDay = groupByDay(messages);
+
+      const wordCountByDay = Object.keys(messagesByDay).map(date => {
+        const count = messagesByDay[date].reduce((total, message) => total + message.content.split(' ').length, 0);
+        return {
+          date: new Date(date), // The date in YYYY-MM-DD format
+          value: count, // The total word count for this date
+          label: moment(date).format('MM/DD') // The label in MM/DD format
+        };
+      });
+
+      // Format and send response
+      return res.status(200).json({
+        lessonsByDay: getCountByDay(lessonsByDay),
+        vocabulariesByDay: getCountByDay(vocabulariesByDay),
+        paraphrasesByDay: getCountByDay(paraphrasesByDay),
+        wordCountByDay: wordCountByDay
+      });
+
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'An error occurred while fetching user stats' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
---
+++ rn/components/UserStats.tsx
@@ -0,0 +1,406 @@
+import { useMemo, useState } from "react";
+import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
+import SelectDropdown from 'react-native-select-dropdown'
+import { Text } from "./Text";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { i18n } from "../locales";
+import moment from "moment";
+
+const sampleData = {
+  "lessonsByDay": [
+    {
+      "date": "2023-11-01T00:00:00.000Z",
+      "label": "11/01",
+      "value": 12
+    },
+    {
+      "date": "2023-10-30T00:00:00.000Z",
+      "label": "10/30",
+      "value": 2
+    },
+    {
+      "date": "2023-11-02T00:00:00.000Z",
+      "label": "11/02",
+      "value": 6
+    },
+    {
+      "date": "2023-11-03T00:00:00.000Z",
+      "label": "11/03",
+      "value": 1
+    },
+    {
+      "date": "2023-11-04T00:00:00.000Z",
+      "label": "11/04",
+      "value": 5
+    },
+    {
+      "date": "2023-11-05T00:00:00.000Z",
+      "label": "11/05",
+      "value": 1
+    },
+    {
+      "date": "2023-11-06T00:00:00.000Z",
+      "label": "11/06",
+      "value": 5
+    },
+    {
+      "date": "2023-11-07T00:00:00.000Z",
+      "label": "11/07",
+      "value": 13
+    },
+    {
+      "date": "2023-11-08T00:00:00.000Z",
+      "label": "11/08",
+      "value": 12
+    },
+    {
+      "date": "2023-11-09T00:00:00.000Z",
+      "label": "11/09",
+      "value": 5
+    },
+    {
+      "date": "2023-11-10T00:00:00.000Z",
+      "label": "11/10",
+      "value": 25
+    },
+    {
+      "date": "2023-11-11T00:00:00.000Z",
+      "label": "11/11",
+      "value": 16
+    },
+    {
+      "date": "2023-11-13T00:00:00.000Z",
+      "label": "11/13",
+      "value": 24
+    },
+    {
+      "date": "2023-11-15T00:00:00.000Z",
+      "label": "11/15",
+      "value": 1
+    },
+    {
+      "date": "2023-11-18T00:00:00.000Z",
+      "label": "11/18",
+      "value": 6
+    },
+    {
+      "date": "2023-11-21T00:00:00.000Z",
+      "label": "11/21",
+      "value": 2
+    },
+    {
+      "date": "2023-11-22T00:00:00.000Z",
+      "label": "11/22",
+      "value": 1
+    }
+  ],
+  "vocabulariesByDay": [
+    {
+      "date": "2023-10-30T00:00:00.000Z",
+      "label": "10/30",
+      "value": 8
+    },
+    {
+      "date": "2023-11-02T00:00:00.000Z",
+      "label": "11/02",
+      "value": 1
+    },
+    {
+      "date": "2023-11-06T00:00:00.000Z",
+      "label": "11/06",
+      "value": 15
+    },
+    {
+      "date": "2023-11-07T00:00:00.000Z",
+      "label": "11/07",
+      "value": 1
+    },
+    {
+      "date": "2023-11-10T00:00:00.000Z",
+      "label": "11/10",
+      "value": 1
+    }
+  ],
+  "paraphrasesByDay": [
+    {
+      "date": "2023-11-02T00:00:00.000Z",
+      "label": "11/02",
+      "value": 1
+    },
+    {
+      "date": "2023-11-10T00:00:00.000Z",
+      "label": "11/10",
+      "value": 2
+    },
+    {
+      "date": "2023-11-16T00:00:00.000Z",
+      "label": "11/16",
+      "value": 1
+    },
+    {
+      "date": "2023-11-22T00:00:00.000Z",
+      "label": "11/22",
+      "value": 1
+    }
+  ],
+  "wordCountByDay": [
+    {
+      "date": "2023-11-01T00:00:00.000Z",
+      "value": 2,
+      "label": "11/01"
+    },
+    {
+      "date": "2023-11-02T00:00:00.000Z",
+      "value": 30,
+      "label": "11/02"
+    },
+    {
+      "date": "2023-11-05T00:00:00.000Z",
+      "value": 34,
+      "label": "11/05"
+    },
+    {
+      "date": "2023-11-06T00:00:00.000Z",
+      "value": 6,
+      "label": "11/06"
+    },
+    {
+      "date": "2023-11-07T00:00:00.000Z",
+      "value": 1,
+      "label": "11/07"
+    },
+    {
+      "date": "2023-11-10T00:00:00.000Z",
+      "value": 22,
+      "label": "11/10"
+    },
+    {
+      "date": "2023-11-18T00:00:00.000Z",
+      "value": 23,
+      "label": "11/18"
+    },
+    {
+      "date": "2023-11-11T00:00:00.000Z",
+      "value": 9,
+      "label": "11/11"
+    },
+    {
+      "date": "2023-11-15T00:00:00.000Z",
+      "value": 5,
+      "label": "11/15"
+    },
+    {
+      "date": "2023-11-16T00:00:00.000Z",
+      "value": 9,
+      "label": "11/16"
+    },
+    {
+      "date": "2023-11-22T00:00:00.000Z",
+      "value": 5,
+      "label": "11/22",
+
+    }
+  ]
+}
+
+enum StastsTab {
+  lessons = 'lessons',
+  vocabulary = 'vocabulary',
+  paraphrase = 'paraphrase',
+  wordCount = 'wordCount',
+}
+
+function convertTabToKey(tab: StastsTab) {
+  switch (tab) {
+    case StastsTab.lessons:
+      return "lessonsByDay"
+    case StastsTab.vocabulary:
+      return "vocabulariesByDay"
+    case StastsTab.paraphrase:
+      return "paraphrasesByDay"
+    case StastsTab.wordCount:
+      return "wordCountByDay"
+  }
+}
+
+const goals: { [key: StastsTab]: number } = {
+  lessons: 3,
+  vocabulary: 20,
+  paraphrase: 10,
+  wordCount: 30,
+}
+
+const addMissingDates = (data, type: StastsTab) => {
+  const dataCopy = [...data]
+  const sortedData = dataCopy.sort((a, b) => new Date(a.date) - new Date(b.date));
+  const startDate = moment(new Date(sortedData[0].date));
+  const endDate = moment(); // Set the end date to today
+  const result = [];
+
+  // Convert data array to a map for faster lookup
+  const dataMap = new Map(dataCopy.map(item => [moment(item.date).format('YYYY-MM-DD'), item]));
+
+  for (let date = moment(startDate); date.isSameOrBefore(endDate); date = date.add(1, 'days')) {
+    const dateString = date.format('YYYY-MM-DD');
+    const found = dataMap.get(dateString);
+    const goal = goals[type];
+    if (found) {
+      result.push({
+        ...found,
+        label: moment(found.date).format('MM/DD'),
+        frontColor: found.value > goal ? '#2FABE8' : null,
+        topLabelComponent: () =>
+          found.value > goal ? (
+            <View style={{ alignItems: "center" }}>
+              <FontAwesome5 name="award" size={18} color="#FFD744" />
+              <Text>{found.value}</Text>
+            </View>
+          ) : <Text>{found.value}</Text>
+      });
+    } else {
+      if (!date.isSame(endDate, 'day')) {
+        result.push({
+          date: dateString,
+          label: date.format('MM/DD'),
+          value: 0 // Default value for missing dates
+        });
+      }
+    }
+  }
+
+  return result;
+};
+
+export function UserStats({ stats = sampleData }) {
+  const [tab, setTab] = useState(StastsTab.lessons)
+
+  const dataToDisplay = useMemo(() => {
+    const key = convertTabToKey(tab)
+    return addMissingDates(stats[key], tab)
+  }, [stats, tab])
+
+  console.log("dataToDisplay", dataToDisplay)
+  return (
+    <View style={{ padding: 24 }}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity onPress={() => setTab(StastsTab.lessons)}>
+          <View style={[styles.button, tab === StastsTab.lessons ? styles.menuButtonActive : null]}>
+            <Text style={[styles.textMenu, tab === StastsTab.lessons ? styles.textMenuActive : null]} weight='SemiBold'>{i18n.t("numberOfLesson")}</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setTab(StastsTab.wordCount)}>
+          <View style={[styles.button, tab === StastsTab.wordCount ? styles.menuButtonActive : null]}>
+            <Text style={[styles.textMenu, tab === StastsTab.wordCount ? styles.textMenuActive : null]} weight='SemiBold'>{i18n.t("numberOfWordSpoken")}</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setTab(StastsTab.vocabulary)}>
+          <View style={[styles.button, tab === StastsTab.vocabulary ? styles.menuButtonActive : null]}>
+            <Text style={[styles.textMenu, tab === StastsTab.vocabulary ? styles.textMenuActive : null]} weight='SemiBold'>{i18n.t("numberOfSavedVocabulary")}</Text>
+          </View>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity onPress={() => setTab(StastsTab.paraphrase)}>
+          <View style={[styles.button, tab === StastsTab.paraphrase ? styles.menuButtonActive : null]}>
+            <Text style={[styles.textMenu, tab === StastsTab.paraphrase ? styles.textMenuActive : null]} weight='SemiBold'>{i18n.t("numberOfSavedParaphrase")}</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
+      <BarChart
+        barWidth={22}
+        noOfSections={5}
+        barBorderRadius={4}
+        frontColor="lightgray"
+        data={dataToDisplay}
+        yAxisThickness={0}
+        xAxisThickness={0}
+      />
+      {/* <View style={{ flexDirection: "row", width: "100%" }}>
+        <SelectDropdown
+          data={displayType}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          renderCustomizedRowChild={(item, index) => {
+            return (
+              <Text>{item.label}</Text>
+            );
+          }}
+          defaultButtonText={'Select city'}
+          dropdownIconPosition={'right'}
+          renderDropdownIcon={isOpened => {
+            return <5 name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#444'} size={18} />;
+          }}
+        />
+        <SelectDropdown
+          data={range}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            // text represented after item is selected
+            // if data array is an array of objects then return selectedItem.property to render after item is selected
+            return selectedItem
+          }}
+          rowTextForSelection={(item, index) => {
+            // text represented for each item in dropdown
+            // if data array is an array of objects then return item.property to represent item in dropdown
+            return item
+          }}
+        />
+      </View> */}
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    width: '100%',
+  },
+  cardWrapper: {
+    marginBottom: 8,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollView: {
+    width: '100%',
+    paddingTop: 28,
+  },
+  scrollViewContainer: {
+    alignItems: 'center',
+  },
+  menu: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: "5%",
+    paddingTop: 12,
+    paddingBottom: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f4f4f4",
+    overflow: 'scroll'
+  },
+  button: {
+    minWidth: 110,
+    padding: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menuButtonActive: {
+    backgroundColor: "#242424",
+    borderRadius: 32,
+    padding: 12,
+  },
+  textMenu: {
+    textAlign: 'center',
+  },
+  textMenuActive: {
+    color: "#fff"
+  }
+});
