ad89c4efea78946829e7807a284f040103c6e48a
Kei <koka0828us@gmail.com>
2023-12-18 12:00:00 +0900

    Update web helpers

--- nextjs/utils/apis/contentful.ts
+++ nextjs/utils/apis/contentful.ts
@@ -3,4 +3,11 @@ const contentful = require('contentful')
 export const client = contentful.createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY,
+  host: process.env.NEXT_PUBLIC_CONTENTFUL_HOST,
+})
+
+console.log({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID?.slice(0, 5),
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY?.slice(0, 5),
+  host: process.env.NEXT_PUBLIC_CONTENTFUL_HOST?.slice(0, 5),
 })
