180831412bb8dd9867a3e27a969cf77ba3402942
Kei <koka0828us@gmail.com>
2023-12-30 12:00:00 +0900

    Add cms api

---
+++ nextjs/pages/api/cms/articles/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getMaterials, createMaterial, getMaterialByUrl, upsertMaterial } from '@/models/material'
+import { getAllArticles, formatEntries } from '@/common/contentful'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    console.log("------------------")
+    const contentfulArticles = await getAllArticles()
+    const items = contentfulArticles.items
+    const articles = formatEntries(items)
+    return res.status(200).json({ articles });
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
