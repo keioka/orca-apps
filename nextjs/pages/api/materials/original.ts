cbdd54017866114e077fa7f8e2af2e21e45a3db9
Kei <koka0828us@gmail.com>
2024-01-10 12:00:00 +0900

    Add include archive

--- nextjs/pages/api/materials/original.ts
+++ nextjs/pages/api/materials/original.ts
@@ -3,7 +3,8 @@ import * as Material from '@/models/material';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const materials = await Material.getOriginalMaterials()
+    const { isIncludeArchived } = req.query
+    const materials = await Material.getOriginalMaterials(!!isIncludeArchived)
     res.status(200).json({ materials })
   } catch (error) {
     console.error(error)
