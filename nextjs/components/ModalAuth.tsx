2dbc4c676c8e58638f7e7af3b3b269a3616591b2
Kei <koka0828us@gmail.com>
2024-02-05 12:00:00 +0900

    Add Line browser invalid logic

--- nextjs/components/ModalAuth.tsx
+++ nextjs/components/ModalAuth.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Modal, Button, Box, TextField, Alert, Stack, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Modal, Button, Box, Alert, Stack, Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { signUpWithGoogle, loginWithGoogle } from "@/redux/features/auth";
 import { IoClose } from "react-icons/io5";
@@ -9,8 +9,14 @@ import { outfit } from '@/font'
 
 export function ModalAuth({ isOpen, onClose, alert }: { isOpen: boolean, onClose: () => void }) {
   const [isSignup, setIsSignup] = useState(false);
+  const [isInvalidBrowser, setIsInvalidBrowser] = useState(false)
+
   const dispatch = useAppDispatch();
   const errorSignupMessage = useAppSelector((state) => state.auth.errorSignupMessage);
+
+  useEffect(() => {
+    setIsInvalidBrowser(window.navigator.userAgent.toLowerCase().includes("line"))
+  }, [])
 
   function onClickGoogleSignup() {
     dispatch(signUpWithGoogle());
@@ -20,9 +26,14 @@ export function ModalAuth({ isOpen, onClose, alert }: { isOpen: boolean, onClose
     dispatch(loginWithGoogle());
   }
 
+  function onOpenBrowser() {
+    window.open(window.location.href, "_system", "location=yes")
+  }
+
   function toggleSignup() {
     setIsSignup(!isSignup);
   }
+
 
   return (
     <Modal
@@ -37,6 +48,8 @@ export function ModalAuth({ isOpen, onClose, alert }: { isOpen: boolean, onClose
         fontFamily: "--var(font-outfit)"
       }}
     >
+
+
       <Box
         style={{
           width: "100%",
@@ -47,85 +60,95 @@ export function ModalAuth({ isOpen, onClose, alert }: { isOpen: boolean, onClose
           boxSizing: "border-box",
         }}
       >
-        <Box onClick={onClose}>
-          <IoClose size={24} style={{ position: "absolute", top: 32, left: 32, cursor: "pointer" }} />
-        </Box>
-        <Box padding={2}>
-          {
-            alert && (
-              <Box mb={2}>
-                <Alert mb={4} color="error">
-                  {alert}
-                </Alert>
-              </Box>
-            )
-          }
-          {isSignup && (
-            <Stack
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-              }}
-              spacing={2}
-            >
-              <Typography
-                variant='h5'
-                sx={{
-                  fontFamily: "--var(font-outfit)"
-                }}
-              >
-                Sign up
-              </Typography>
-              <Stack spacing={1}>
-                <Button variant='contained' onClick={onClickGoogleSignup} size='large' sx={{ color: "#fff" }}>
-                  <Image
-                    src={GoogleLogo}
-                    alt="Google logo"
-                    width={24}
-                    height={24}
-                  />
-                  Sign up with Google
-                </Button>
-                <Button onClick={toggleSignup}>Login</Button>
-              </Stack>
-            </Stack>
-          )}
 
-          {!isSignup &&
-            <Stack
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-              }}
-              spacing={2}
-            >
-              <Typography variant='h5'>Login</Typography>
-              <Stack spacing={1}>
-                <Button
-                  variant='outlined'
-                  onClick={onClickGoogleLogin}
-                  sx={{
-                    background: "#fff",
-                    fontFamily: "--var(font-outfit)",
-                  }}
-                  size='large'
-                >
-                  <Image
-                    src={GoogleLogo}
-                    alt="Google logo"
-                    width={24}
-                    height={24}
-                  />
-                  Login with Google
-                </Button>
-                <Button onClick={toggleSignup}>Create a new account</Button>
-                <Typography>{errorSignupMessage}</Typography>
+
+        <>
+          <Box onClick={onClose}>
+            <IoClose size={24} style={{ position: "absolute", top: 32, left: 32, cursor: "pointer" }} />
+          </Box>
+          <Box padding={2}>
+            {
+              alert && (
+                <Box mb={2}>
+                  <Alert mb={4} color="error">
+                    {alert}
+                  </Alert>
+                </Box>
+              )
+            }
+            {isInvalidBrowser && (
+              <Stack sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} spacing={2} mt={3}>
+                <Typography>LINE内ブラウザではログインできません。他のブラウザをご利用ください。</Typography>
+                <Button onClick={onOpenBrowser} variant="outlined">別のブラウザで開く</Button>
               </Stack>
-            </Stack>
-          }
-        </Box>
+            )}
+            {isSignup && (
+              <Stack
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+                spacing={2}
+              >
+                <Typography
+                  variant='h5'
+                  sx={{
+                    fontFamily: "--var(font-outfit)"
+                  }}
+                >
+                  Sign up
+                </Typography>
+                <Stack spacing={1}>
+                  <Button variant='contained' onClick={onClickGoogleSignup} size='large' sx={{ color: "#fff" }}>
+                    <Image
+                      src={GoogleLogo}
+                      alt="Google logo"
+                      width={24}
+                      height={24}
+                    />
+                    Sign up with Google
+                  </Button>
+                  <Button onClick={toggleSignup}>Login</Button>
+                </Stack>
+              </Stack>
+            )}
+
+            {!isSignup &&
+              <Stack
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+                spacing={2}
+              >
+                <Typography variant='h5'>Login</Typography>
+                <Stack spacing={1}>
+                  <Button
+                    variant='outlined'
+                    onClick={onClickGoogleLogin}
+                    sx={{
+                      background: "#fff",
+                      fontFamily: "--var(font-outfit)",
+                    }}
+                    size='large'
+                  >
+                    <Image
+                      src={GoogleLogo}
+                      alt="Google logo"
+                      width={24}
+                      height={24}
+                    />
+                    Login with Google
+                  </Button>
+                  <Button onClick={toggleSignup}>Create a new account</Button>
+                  <Typography>{errorSignupMessage}</Typography>
+                </Stack>
+              </Stack>
+            }
+          </Box>
+        </>
       </Box>
     </Modal >
   );
