ddfa6675ce7d48f69bd5b0b4ed60de67b69e64c5
Kei <koka0828us@gmail.com>
2024-02-06 12:00:00 +0900

    Update web pages

--- nextjs/pages/_document.tsx
+++ nextjs/pages/_document.tsx
@@ -8,6 +8,19 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
+        {process.env.APP_ENV == 'production' &&
+          (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-NE6PCSKN7W"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date())
+                gtag('config', 'G-NE6PCSKN7W');
+              </script>
+            </>
+          )
+        }
       </Head>
       <body>
         <Main />
