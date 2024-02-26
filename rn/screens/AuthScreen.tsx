ce42ce4abf13a0d7a203b322d1435df106f8e2ac
Kei <koka0828us@gmail.com>
2024-02-26 12:00:00 +0900

    Fix config and add Google Auth Buttons

--- rn/screens/AuthScreen.tsx
+++ rn/screens/AuthScreen.tsx
@@ -87,6 +87,9 @@ export function AuthScreen({ navigation }) {
             <Button disabled={singinLoading} onPress={handleSignin} mode="contained" disabled={isSigninDisabled}>{i18n.t("loginAction")}</Button>
           </View>
           <View style={styles.verticallySpaced}>
+            <Button onPress={() => { }} mode="text">Google signin</Button>
+          </View>
+          <View style={styles.verticallySpaced}>
             <Button onPress={() => setSignin(false)} mode="text" >{i18n.t("signupAction")}</Button>
           </View>
         </>
@@ -136,8 +139,13 @@ export function AuthScreen({ navigation }) {
             <Button disabled={signupLoading} onPress={handleSignup} mode="contained" disabled={isSignupDisabled}>{i18n.t("signupAction")}</Button>
           </View>
           <View style={styles.verticallySpaced}>
+            <Button onPress={() => { }} mode="text">Google signup</Button>
+          </View>
+          <View style={styles.verticallySpaced}>
             <Button onPress={() => setSignin(true)} mode="text">{i18n.t("loginAction")}</Button>
           </View>
+
+
         </>
         }
       </KeyboardAvoidingView>
