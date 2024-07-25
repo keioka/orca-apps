1e27a341eb42df3ce11947eaa94a8f462ec78700
Kei <koka0828us@gmail.com>
2024-07-25 12:00:00 +0900

    Add ce vocab

--- ce/contents/plasmo-root-container.tsx
+++ ce/contents/plasmo-root-container.tsx
@@ -1,21 +1,29 @@
-import type { PlasmoCSConfig } from "plasmo"
 import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
+import { Drawer } from "@mui/material"
+import ScopedCssBaseline from "@mui/material/ScopedCssBaseline"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { Inject } from "components/Inject"
-import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { InjectVocab } from "components/InjectVocab"
 import cssText from "data-text:~/global.css"
-import { Provider } from "react-redux";
+import mixpanel from "mixpanel-browser"
+import type {
+  PlasmoCSConfig,
+  PlasmoCSUIJSXContainer,
+  PlasmoMountShadowHost,
+  PlasmoRender
+} from "plasmo"
+import { useEffect, useMemo } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+import { Provider } from "react-redux"
+
 import { PersistGate } from "@plasmohq/redux-persist/integration/react"
-import { persistor, store } from '~/redux/store';
-import { getTheme } from "~/theme";
-import { ErrorBoundary } from "react-error-boundary";
-import { useEffect, useMemo } from "react";
-import { useAppDispatch } from "~redux/hooks";
-import mixpanel from "mixpanel-browser";
-import { fetchDeviceId } from '~/utils/fetchDeviceId'
-import { Drawer } from "@mui/material";
-import { LocaleProvider } from "~hooks/locale";
+
+import { persistor, store } from "~/redux/store"
+import { getTheme } from "~/theme"
+import { fetchDeviceId } from "~/utils/fetchDeviceId"
+import { LocaleProvider } from "~hooks/locale"
+import { useAppDispatch } from "~redux/hooks"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://*/*", "http://*/"],
@@ -36,7 +44,6 @@ export const getStyle = () => {
 }
 
 function Root() {
-
   const theme = useMemo(() => {
     if (!chrome) return getTheme("en")
     const lang = chrome.i18n.getUILanguage()
@@ -45,16 +52,17 @@ function Root() {
     return theme
   }, [chrome])
 
+  console.log("Root")
   useEffect(() => {
     console.log("=====================================")
-    mixpanel.init(process.env.PLASMO_PUBLIC_MIXPANEL_TOKEN, { track_pageview: false });
+    mixpanel.init(process.env.PLASMO_PUBLIC_MIXPANEL_TOKEN, {
+      track_pageview: false
+    })
 
     async function setMixpanelUUID() {
       const uuid = await fetchDeviceId()
-      console.log({ uuid })
       mixpanel.identify(uuid)
     }
-
     setMixpanelUUID()
   }, [])
 
@@ -63,18 +71,14 @@ function Root() {
       <LocaleProvider>
         <ThemeProvider theme={theme}>
           <Provider store={store}>
-            <PersistGate
-              loading={null}
-              persistor={persistor}
-            >
+            <PersistGate loading={null} persistor={persistor}>
               <ScopedCssBaseline
                 sx={{
                   backgroundColor: "transparent",
-                  pointerEvents: "none",
-                }}
-              >
+                  pointerEvents: "none"
+                }}>
                 <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-                  <Inject />
+                  <InjectVocab />
                 </ErrorBoundary>
               </ScopedCssBaseline>
             </PersistGate>
@@ -86,13 +90,16 @@ function Root() {
 }
 
 const drawerWidth = 380
-function ErrorBoundaryFallback({ error, resetErrorBoundary }): React.JSX.Element {
+function ErrorBoundaryFallback({
+  error,
+  resetErrorBoundary
+}): React.JSX.Element {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     console.log("ErrorBoundaryFallback")
     console.error(error)
-    dispatch({ type: "global/RESET_STATE" });
+    dispatch({ type: "global/RESET_STATE" })
   }, [])
 
   return (
@@ -107,38 +114,14 @@ function ErrorBoundaryFallback({ error, resetErrorBoundary }): React.JSX.Element
         "& .MuiDrawer-paper": {
           width: drawerWidth,
           boxSizing: "border-box",
-          background: "#fff",
+          background: "#fff"
         }
-      }}
-    >
+      }}>
       <h1>Something went wrong</h1>
       <pre>{error.message}</pre>
       <button onClick={resetErrorBoundary}>Try again</button>
     </Drawer>
   )
 }
-
-// export const getRootContainer = () =>
-//   new Promise((resolve) => {
-//     const checkInterval = setInterval(() => {
-//       const rootContainerParent = document.querySelector(`[href="/docs"]`)
-//       if (rootContainerParent) {
-//         clearInterval(checkInterval)
-//         const rootContainer = document.createElement("div")
-//         rootContainerParent.appendChild(rootContainer)
-//         resolve(rootContainer)
-//       }
-//     }, 137)
-//   })
-
-
-// export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
-//   createRootContainer
-// }) => {
-//   const rootContainer = await createRootContainer()
-//   const root = createRoot(rootContainer)
-//   root.render(<Root />)
-// }
-
 
 export default Root
